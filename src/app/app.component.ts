2c040b630f4d1d2c62f2c706c201aad2637f4dcf
Manpreet-Tiwari <105644197+Manpreet-Tiwari@users.noreply.github.com>
2022-06-27 17:45:28 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Td-Form';
+  genders=['Male', 'Female', 'Cannot Say'];
+
+  suggestUserName(){
+    const suggestUserName ="Superuser"
+  }
+
+  onSubmit(form : NgForm){
+    console.log(form);
+    console.log(form.value);
+    console.log(form.status)
+  }
 }
